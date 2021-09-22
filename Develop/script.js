d7abbb682cb2da96c495792db4518710780d4ad4
Jess Hause Ugray <jess.ugray@gmail.com>
2021-09-22 10:33:51 -0400

    function added using moment js to check the time and update the css accordingly

--- Develop/script.js
+++ Develop/script.js
@@ -1,74 +1,120 @@
+// keep ID in each index doc but then have the click event find a generic button class, then find the specific ID of the form input
 
+//when a save button is clicked, the text input is saved to localstorage
 $("#btn9").click(function(event) {
   event.preventDefault();
-  var textInput = $("#calText9").val(); 
+  var textInput = $("#09").val(); 
   console.log(textInput);
   localStorage.setItem("savedCalEntry9", JSON.stringify(textInput));
 });
 
 $("#btn10").click(function(event) {
   event.preventDefault();
-  var textInput = $("#calText10").val(); 
+  var textInput = $("#10").val(); 
   console.log(textInput);
   localStorage.setItem("savedCalEntry10", JSON.stringify(textInput));
 });
 
 $("#btn11").click(function(event) {
   event.preventDefault();
-  var textInput = $("#calText11").val(); 
+  var textInput = $("#11").val(); 
   console.log(textInput);
   localStorage.setItem("savedCalEntry11", JSON.stringify(textInput));
 });
 
 $("#btn12").click(function(event) {
   event.preventDefault();
-  var textInput = $("#calText12").val(); 
+  var textInput = $("#12").val(); 
   console.log(textInput);
   localStorage.setItem("savedCalEntry12", JSON.stringify(textInput));
 });
 
 $("#btn13").click(function(event) {
   event.preventDefault();
-  var textInput = $("#calText13").val(); 
+  var textInput = $("#13").val(); 
   console.log(textInput);
   localStorage.setItem("savedCalEntry13", JSON.stringify(textInput));
 });
 
 $("#btn14").click(function(event) {
   event.preventDefault();
-  var textInput = $("#calText14").val(); 
+  var textInput = $("#14").val(); 
   console.log(textInput);
   localStorage.setItem("savedCalEntry14", JSON.stringify(textInput));
 });
 
 $("#btn15").click(function(event) {
   event.preventDefault();
-  var textInput = $("#calText15").val(); 
+  var textInput = $("#15").val(); 
   console.log(textInput);
   localStorage.setItem("savedCalEntry15", JSON.stringify(textInput));
 });
 
 $("#btn16").click(function(event) {
   event.preventDefault();
-  var textInput = $("#calText16").val(); 
+  var textInput = $("#16").val(); 
   console.log(textInput);
   localStorage.setItem("savedCalEntry16", JSON.stringify(textInput));
 });
 
 $("#btn17").click(function(event) {
   event.preventDefault();
-  var textInput = $("#calText17").val(); 
+  var textInput = $("#17").val(); 
   console.log(textInput);
   localStorage.setItem("savedCalEntry17", JSON.stringify(textInput));
 });
 
+// retrieves the info from localstorage and loads it on the page
 
-$("#calText9").val(JSON.parse(localStorage.getItem("savedCalEntry9")));
-$("#calText10").val(JSON.parse(localStorage.getItem("savedCalEntry10")));
-$("#calText11").val(JSON.parse(localStorage.getItem("savedCalEntry11")));
-$("#calText12").val(JSON.parse(localStorage.getItem("savedCalEntry12")));
-$("#calText13").val(JSON.parse(localStorage.getItem("savedCalEntry13")));
-$("#calText14").val(JSON.parse(localStorage.getItem("savedCalEntry14")));
-$("#calText15").val(JSON.parse(localStorage.getItem("savedCalEntry15")));
-$("#calText16").val(JSON.parse(localStorage.getItem("savedCalEntry16")));
-$("#calText17").val(JSON.parse(localStorage.getItem("savedCalEntry17")));
+$("#09").val(JSON.parse(localStorage.getItem("savedCalEntry9")));
+$("#10").val(JSON.parse(localStorage.getItem("savedCalEntry10")));
+$("#11").val(JSON.parse(localStorage.getItem("savedCalEntry11")));
+$("#12").val(JSON.parse(localStorage.getItem("savedCalEntry12")));
+$("#13").val(JSON.parse(localStorage.getItem("savedCalEntry13")));
+$("#14").val(JSON.parse(localStorage.getItem("savedCalEntry14")));
+$("#15").val(JSON.parse(localStorage.getItem("savedCalEntry15")));
+$("#16").val(JSON.parse(localStorage.getItem("savedCalEntry16")));
+$("#17").val(JSON.parse(localStorage.getItem("savedCalEntry17")));
+
+
+var now = moment();
+console.log(now);
+
+//function to check the time, and update the css as needed based on the time
+var confirmTime = function () {
+
+  //defines the current time using moment.js, HH= 09, 10...17 format
+  var currentTime = moment().format('HH');
+  console.log(currentTime);
+
+  //selects the element with the class of textarea (the rows)
+  var calendarEl = $(".textarea");
+  console.log(calendarEl)
+
+  //for loop to go through each calendarEL row until the end, comparing the currentTime to the time in the calendar/schedule
+  for (var i = 0 ; i < calendarEl.length ; i++) {
+
+    //each calendarEL has an ID with the time for that calendar position, this identifies them
+      var timeID = calendarEl[i].id;
+      console.log(timeID)
+
+      //selects the element by ID that we will later update based on the time
+      var calID = document.getElementById(calendarEl[i].id)
+      console.log(calID)
+
+      //as the function will be repeated to update, this is needed to remove outdated past/present/future classes
+      $(calendarEl[i].id).removeClass(".present .past .future");
+
+     // loops through updating the css class based on the time
+      if (timeID < currentTime) {
+          $(calID).addClass("past");
+      } else if (timeID > currentTime) {
+          $(calID).addClass("future");
+      } else {
+          $(calID).addClass("present");
+      }
+  }
+}
+
+// reruns the confirmTime function to update the calendar without the need for the user to refresh the page
+setInterval(confirmTime(), 50000);
